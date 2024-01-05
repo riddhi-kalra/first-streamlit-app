73b3a44550da2676fdac6f703680d066ca6b344f
riddhi-kalra <155716248+riddhi-kalra@users.noreply.github.com>
2024-01-05 20:29:39 +0530

    Added breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title('Hey Its 2024');
 streamlit.header('We will eat Healthy!!')
-streamlit.text('So here we have our first Meal')
-streamlit.text('Oats Meal with Blueberry')
+streamlit.text('Breakfast Menu 🍞')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Tost')
