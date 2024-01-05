1caa6df3a36006edf23e70a10b32a85e2bc4831e
riddhi-kalra <155716248+riddhi-kalra@users.noreply.github.com>
2024-01-05 20:21:50 +0530

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('Hey Its 2024');
 streamlit.header('We will eat Healthy!!')
-streamlit.text('So here we have out first Meal')
+streamlit.text('So here we have our first Meal')
 streamlit.text('Oats Meal with Blueberry')
