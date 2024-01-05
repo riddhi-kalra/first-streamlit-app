49658a50255f544f5d9b9431eb31192e5b82d366
riddhi-kalra <155716248+riddhi-kalra@users.noreply.github.com>
2024-01-05 20:20:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('Hey Its 2024');
+streamlit.header('We will eat Healthy!!')
+streamlit.text('So here we have out first Meal')
+streamlit.text('Oats Meal with Blueberry')
