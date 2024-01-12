909e83ed3a460d1c54db5fe9f0f45a2b57c6bfad
riddhi-kalra <155716248+riddhi-kalra@users.noreply.github.com>
2024-01-12 15:06:38 +0530

    insert value

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,9 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The food load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(add_my_fruit)")
+streamlit.write('Thanks for adding ', fruit_choice)
+
